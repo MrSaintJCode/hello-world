6c60a40d77098bcb51770e1640a80c98339ed799
mrsaintjcode <mrsaintj@DESKTOP-7PA38VS.localdomain>
2020-08-25 09:53:19 -0400

    More Robust with Args

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+container=$1
+image=$2
+
 echo "[+] Checking Running Containers..."
 
-container_name=$(docker ps -aqf "name=devops-container")
+container_name=$(docker ps -aqf "name=$container")
 if [[ "$container_name" == "" ]]; then
         echo "[*] Docker Container Doesn't Currently Exist"
 else
@@ -19,16 +22,18 @@ else
         fi
 fi
 
+sleep 2
+
 echo "[+] Checking for Duplicate Images..."
 
-image_name=$(docker images -q devops-project)
+image_name=$(docker images -q $image)
 if [[ "$image_name" == "" ]]; then
         echo "[*] Docker Image Doesn't Currently Exist"
 else
         echo "[*] Docker Image Exist"
         echo "[+] Attempting to Remove Image"
         docker rmi $image_name
-        image_name=$(docker images -q devops-project)
+        image_name=$(docker images -q $image)
         if [[ "$image_name" == "" ]]; then
                 echo "[*] Docker Image Removed"
         else
